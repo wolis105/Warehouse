8ce7e4bae091a74194a8340c8005bda6c4c167f6
admin <admin@DESKTOP-G6LQT32>
2017-04-18 19:36:10 +0800

    微改

--- Warehouse/Warehouse/Program.cs
+++ Warehouse/Warehouse/Program.cs
@@ -16,7 +16,7 @@ namespace Warehouse
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmExtractDetail());
+            Application.Run(new Form1());
         }
     }
 }
