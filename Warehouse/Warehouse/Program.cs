20452d1f3a243987a63d8dd60e044379b9beb641
lzs <lzs@LAPTOP-RO4TI2GI>
2017-04-18 23:48:50 +0800

    修改1

--- Warehouse/Warehouse/Program.cs
+++ Warehouse/Warehouse/Program.cs
@@ -16,7 +16,7 @@ namespace Warehouse
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmGoods());
+            Application.Run(new Form1());
         }
     }
 }
